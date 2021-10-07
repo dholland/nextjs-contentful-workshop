b1a338d43955a6745aadcb1667fa7a64d9eec639
Danny Holland <holland.danny@gmail.com>
2021-10-07 11:50:09 -0400

    inital commit

--- src/features/layout/index.jsx
+++ src/features/layout/index.jsx
@@ -2,14 +2,16 @@ import Container from '@/ui/container';
 
 export default function Layout({ children }) {
 	return (
-		<>
+		<div className='min-h-screen flex flex-col'>
 			<header className='bg-gray-700 text-white p-8'>
-				<Container>Bloom MT</Container>
+				<Container>
+					<nav className='text-center text-5xl'>Blog</nav>
+				</Container>
 			</header>
-			{children}
+			<div className='flex-grow'>{children}</div>
 			<footer className='bg-gray-300 text-white p-8'>
 				<Container>footer</Container>
 			</footer>
-		</>
+		</div>
 	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import Page from '@/ui/page';
 export default function Home() {
 	return (
 		<Page title='Page Title' description='My Starter Nextjs with Tailwind JIT'>
-			<main className='flex flex-col h-full bg-red-200'>
-				<h1 className='text-6xl text-center '>
-					NextJs Starter with Tailwind JIT
-				</h1>
+			<main className='flex flex-col'>
+				<h1 className='text-6xl text-center'>Content</h1>
 			</main>
 		</Page>
 	);
